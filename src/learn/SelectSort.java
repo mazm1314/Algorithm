ef5b633aa4382475316bc02e8c1d9a30340813c5
mazm <mazm@ziroom.com>
2020-03-01 19:14:28 +0800

    提交注释

--- src/learn/SelectSort.java
+++ src/learn/SelectSort.java
@@ -21,11 +21,15 @@ public class SelectSort {
         int len = arr.length;
         for (int i = 0; i < len - 1; i++) {
             int minshort = i;
+            //j控制比较次数，因为每次循环结束之后最小的数已经放在了最开始的地方
+            //所以下一次循环的时候可以跳过这个数，所以j的初始值为i+1，
             for (int j = i + 1; j < len; j++) {
+                //比较每一次都需要将较小的下标记录下来
                 if (arr[minshort] > arr[j]) {
                     minshort = j;
                 }
             }
+            //当完成一次循环后，就需要将本次循环选取最小的值移动到本次最开始的地方
             if (minshort != i) {
                 int temp = arr[i];
                 arr[i] = arr[minshort];
